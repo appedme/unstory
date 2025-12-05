874652bd1524ac809d1721e5a3f3c9044f7f78b9
SH20RAJ <shaswatraj3@gmail.com>
2025-12-05 13:41:40 +0530

    feat: Add `@stackframe/stack` integration with new client/server components, a stack handler, a loading UI, and update project configuration.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { StackProvider, StackTheme } from "@stackframe/stack";
+import { stackClientApp } from "../stack/client";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,7 +29,7 @@ export default function RootLayout({
 			<head>
 				<link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
 			</head>
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}><StackProvider app={stackClientApp}><StackTheme>{children}</StackTheme></StackProvider></body>
 		</html>
 	);
 }
